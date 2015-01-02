27d23388c0077448c1d3d44fd61b979c5e0e3354
Pablo Fernandez <fernandezpablo85@gmail.com>
2015-01-02 15:55:06 -0300

    try adding some dependencies with godep

--- main.go
+++ main.go
@@ -1,5 +1,20 @@
 package main
 
+import (
+	"code.google.com/p/rsc/qr"
+	"io/ioutil"
+  "flag"
+)
+
 func main() {
-  println("hello Go!")
+  content := flag.String("content", "default", "the content to be encoded")
+  fileName := flag.String("out", "qr", "the name of the outputted qr file")
+
+  flag.Parse()
+
+	code, err := qr.Encode(*content, qr.L)
+  if err != nil {
+    panic("error while encoding contents")
+  }
+	ioutil.WriteFile(*fileName + ".png", code.PNG(), 0644)
 }
